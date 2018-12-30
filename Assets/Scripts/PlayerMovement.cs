cc41d6cac3f1c7fe31151424805c120cf501c847
zachary rowden <zacharyrowden@zacharys-MacBook-Pro.local>
2018-12-30 02:03:31 +0100

    Moving character but not logical.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour {
 
 	private float leftWall = -4f;
 	private float rightWall = 4f;
+	private Animator anim;
 	// Use this for initialization
 	void Start () {
 	
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -20,10 +22,27 @@ public class PlayerMovement : MonoBehaviour {
 		//make it move per seconds instead of frames
 		float translation = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
 
+		if (translation > 0) {
+			transform.localScale = new Vector3 (1, 1, 1);
+		} 
+		else if(translation < 0) {
+			transform.localScale = new Vector3 (-1, 1, 1);			
+		}
+
 		if (transform.position.x + translation < rightWall && transform.position.x + translation > leftWall)
 			transform.Translate (translation, 0, 0);
 		
-	
+	    //switching between idale and walk states in anim
+		if (translation != 0) {
+			anim.SetFloat("PlayerSpeed", speed);
+		} else {
+			anim.SetFloat ("PlayerSpeed", 0);
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			anim.SetBool("Jump", !(anim.GetBool("Jump")));
+		}
 	
 	}
+		
 }
