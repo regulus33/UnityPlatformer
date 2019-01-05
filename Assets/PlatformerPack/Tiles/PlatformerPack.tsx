44cd226d4f23e0aa692be4288ecd3e8a5f8739c7
zachary rowden <zacharyrowden@zacharys-MacBook-Pro.local>
2019-01-05 16:59:15 +0100

    Fix collider in level import.

---
+++ Assets/PlatformerPack/Tiles/PlatformerPack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="/Users/zacharyrowden/BluePrint/Assets/PlatformerPack" tilewidth="70" tileheight="70" tilecount="169" columns="13">
+ <image source="tiles_spritesheet.png" width="914" height="936"/>
+</tileset>
---
+++ Assets/PlatformerPack/Tiles/tiles_spritesheet.tsx
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tiles_spritesheet" tilewidth="71" tileheight="71" spacing="1" tilecount="156" columns="12">
+ <image source="tiles_spritesheet.png" width="914" height="936"/>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+   <object id="2" x="71" y="0"/>
+  </objectgroup>
+ </tile>
+ <tile id="91">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+   <object id="2" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="115">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="127">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="128">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+   <object id="2" x="0" y="0" width="71" height="71"/>
+   <object id="5" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="132">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="136">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="139">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="141">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+ <tile id="152">
+  <objectgroup draworder="index">
+   <object id="30" x="0" y="0" width="71" height="71"/>
+  </objectgroup>
+ </tile>
+</tileset>
