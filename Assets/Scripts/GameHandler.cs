7629193bf19b01a9af018c405d87721ba93e8873
zachary rowden <zacharyrowden@zacharys-MacBook-Pro.local>
2019-01-06 13:42:46 +0100

    Finish basic collider scripts, game over and end game logic.

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHandler : MonoBehaviour {
+
+	public float health = 2;
+	public float score = 0;
+	public bool gameover = false;
+
+	public UnityEngine.UI.Text healthUI;
+	public UnityEngine.UI.Text ScoreUI;
+
+	public GameObject youWinUI;
+	public GameObject gameOverUI;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		healthUI.text = health.ToString ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D c) {
+		if (c.tag == "Coin") {
+			
+			AddScore (); 
+			Destroy (c.gameObject);
+
+		} else if (c.tag == "Water") {
+			
+			health = 0;
+			healthUI.text = health.ToString ();	
+			gameOverUI.SetActive (true);
+			StopGame ();
+
+
+		} else if ( c.tag == "Ending" ) {
+			youWinUI.SetActive (true);
+			StopGame ();
+		}
+	}
+
+	public void SubtractHealth() {
+		health -= 1;
+		healthUI.text = health.ToString ();
+		if (health == 0) {
+			gameOverUI.SetActive (true);
+			StopGame ();
+		}
+
+	}
+
+	public void AddScore() {
+		score += 10;
+		ScoreUI.text = score.ToString();
+	}
+
+	public void StopGame() {
+		gameover = true;
+		gameObject.SetActive(false);
+	}
+
+}
